a25caa02f981c848ba7153e4642da48ff5d2f02f
Sofia Chaudhry <sofia.chaudhry2606@gmail.com>
2023-10-04 19:04:15 +0200

    feature/front-06 - Auth user
    Fix regex middleware to catch /account in routes and redirect to the login page

--- front/src/middleware.js
+++ front/src/middleware.js
@@ -1,8 +1,7 @@
 import {withAuth} from "next-auth/middleware";
 
 export function middleware(request) {
-    // if(/\/[a-z]{2}\/account.*/.test(request.nextUrl.pathname)) {
-    if(/\/*\/account.*/.test(request.nextUrl.pathname)) {
+    if(/.*(account)+.*/.test(request.nextUrl.pathname)) {
         return withAuth(request);
     }
 }
