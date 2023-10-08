98c202bd1ced8b5d5f278aa5f38345f1d99b527d
Kba <struys.wyvin@gmail.com>
2023-10-08 20:31:35 +0200

    feature/front-10
    - add table

--- front/src/app/admin/products/page.js
+++ front/src/app/admin/products/page.js
@@ -1,12 +1,27 @@
-import TableAdmin from "../../_components/table/tableAdmin"
+'use client';
+import {getProducts} from "@/app/utils/products";
+import {useEffect, useState} from "react";
+import TableAdmin from "@/app/_components/table/tableAdmin";
 
 export default function AdminProducts() {
+
+    const [products = [], setProducts] = useState();
+
+    const fetchData = async () => {
+        const result = await getProducts('articles');
+        setProducts(result);
+    }
+    useEffect(() => {
+        fetchData()
+            .catch(e => console.log(e));
+    }, []);
+
     return (
         <div>
             <h1 className="text-center my-10">Gestion des Produits !</h1>
             <div>
                 <h2 className="text-center">Des produits ??</h2>
-                <TableAdmin data={"test"} />
+                <TableAdmin data={products} />
             </div>
         </div>
     )
--- front/src/app/utils/products.js
+++ front/src/app/utils/products.js
@@ -1,8 +1,21 @@
 import axios from "axios";
 
+const API_URL = "http://localhost:8000/api/"
+
 export async function getProductById(id) {
+
     try {
         const response = await axios.get(`https://dummyjson.com/products/${id}`);
+        return response.data;
+    } catch (e)
+    {
+        throw new Error(e);
+    }
+}
+
+export async function getProducts(param) {
+    try {
+        const response = await axios.get(`${API_URL}${param}`);
         return response.data;
     } catch (e)
     {
